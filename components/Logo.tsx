b074c74ad372b8f7fc3201b0bacde151f0e4038b
Madhanagopal12 <Madhanagopal2004@gmail.com>
2026-03-11 20:37:02 +0530

    initial form builder setup

---
+++ components/Logo.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+function Logo() {
+  return (
+    <Link
+      href={"/"}
+      className="font-bold text-3xl bg-linear-to-r from bg-indigo-400 to cyan-400 text-transparent 
+      bg-clip-text hover:cursor-pointer"
+    >
+      Form Builder
+    </Link>
+  );
+}
+
+export default Logo;
